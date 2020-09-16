9709895598c88494b3e7ea7f88918f4c75713fab
mkeshav218 <mishrakeshav218@gmail.com>
2020-09-16 19:43:11 +0530

    Arrays

--- lesson02/CyclicRotation.java
+++ lesson02/CyclicRotation.java
@@ -1,6 +1,8 @@
 class Solution { 
 	public int[] solution(int[] A, int K) {
 		int n = A.length;
+                                      if(n==0)
+                                             return A;
 		int[] B = new int[n];
 		K = K % n;
 		for(int i=0;i<n;i++) {

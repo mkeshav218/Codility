dcb8e9a101d130a6e078ee912e577c6a518c42df
mkeshav218 <mishrakeshav218@gmail.com>
2020-09-16 20:25:05 +0530

    Prefix Sums

--- lesson05/MinAvgTwoSlice.java
+++ lesson05/MinAvgTwoSlice.java
@@ -1,17 +1,25 @@
 class Solution { 
 	public int solution(int[] A) {
-		int total=0,max=0;
-		for(int i=A.length-1;i>=0;i--) {
-			if(A[i]==1) {
-				max++;
-			}else {
-				total += max;
-			}
-			if(total>1000000000) {
-				return -1;
-			}
-		}
-		
-		return total;
+		double minAvg =Double.MAX_VALUE;
+		 double sum1,sum2;
+		 int minIndex=-1;
+		 for(int i=0;i<A.length-2;i++) {
+			 sum1 = A[i]+A[i+1];
+			 if(sum1/2 <minAvg) {
+				 minAvg = sum1/2;
+				 minIndex=i;
+			 }
+			 sum2 = sum1 + A[i+2];
+			 if(sum2/3 < minAvg) {
+				 minAvg = sum2/3;
+				 minIndex=i;
+			 }
+		 }
+		 sum1 = A[A.length-2]+A[A.length-1];
+		 if(sum1/2 < minAvg) {
+			 minAvg =sum1/2;
+			 minIndex = A.length-2;
+		 }
+		 return minIndex;
 	}
 }

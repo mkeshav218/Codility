6119f501a7d3e6ffb923f837edcfef6d7f925901
mkeshav218 <mishrakeshav218@gmail.com>
2020-09-17 10:24:56 +0530

    Euclidean algorithm

---
+++ lesson12/ChocolatesByNumbers.java
@@ -0,0 +1,12 @@
+class Solution{
+	public int solution(int N, int M) {
+		int k = gcd(N,M);
+		int noOfChocolate = N/k;
+		return noOfChocolate;
+	}
+	private static int gcd(int N,int M) {
+		if(N%M==0)
+			return M;
+		else 
+			return gcd(M,N%M);
+	}

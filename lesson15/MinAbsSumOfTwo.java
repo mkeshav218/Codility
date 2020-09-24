e1c7c5289dc19acdef9b052a9fc475bc9a72ad5a
mkeshav218 <mishrakeshav218@gmail.com>
2020-09-24 16:40:02 +0530

    Caterpillar method

--- lesson15/MinAbsSumOfTwo.java
+++ lesson15/MinAbsSumOfTwo.java
@@ -1,28 +1,31 @@
-class Solution{
-	public int solution(int[] A) {
-		 int result = Integer.MAX_VALUE;
-		 Arrays.sort(A);
-		 if(A[0]>=0)
-			 return A[0]+A[0];
-		 else if(A[A.length-1]<=0) {
-			 return Math.abs(A[A.length-1]+A[A.length-1]);
-		 }else {
-			 int i=0,j=A.length-1,k=1;
-			 while(A[k]<0 && (Math.abs(Math.abs(A[i])-A[j]))>(Math.abs(Math.abs(A[k])-A[j]))) {
-		
-				 i=k;
-				 k++;
-			 }
-			 result = Math.min(Math.abs(A[i]+A[j]),Math.abs(A[k]+A[k]));
-			 int firstpositiveindex=0;
-			 for(i=1;i<A.length;i++) {
-				 if(A[i]>=0) {
-					 firstpositiveindex = i;
-					 break;
-				 }
-			 }
-			 result = Math.min(result,Math.abs(A[firstpositiveindex]+A[firstpositiveindex]));
-		 }
-		 return result;
-	}
+class Solution {
+    public int solution(int[] A) {
+        if(A.length==1)
+        return Math.abs(2*A[0]);
+        List<Integer> b = new ArrayList<Integer>();
+		for(int i:A){
+		    b.add(i);
+		}
+		Collections.sort(b,new MyComparator());
+		int minSum=Integer.MAX_VALUE;
+		for(int i=0;i<b.size()-1;i++){
+		    int k = Math.abs(b.get(i)+b.get(i));
+		    if(minSum>k){
+		        minSum=k;
+		    }
+		    k = Math.abs(b.get(i)+b.get(i+1));
+		    if(minSum>k){
+		        minSum=k;
+		    }
+		}
+		return minSum;
+    }
+}
+class MyComparator implements Comparator<Integer> {
+    
+    public int compare(Integer a, Integer b){
+        if(a.equals(b))
+        return 1;
+        return Math.abs(a)- Math.abs(b);
+    }
 }
